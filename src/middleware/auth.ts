80474d8588c682ae5ac7db21ce5c208e3b3c8524
mdshoaib77 <email4966@gmail.com>
2026-06-01 10:50:22 +0600

    ALhamdulliah anoter validation add

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,65 @@
+// import type { NextFunction, Request, Response } from "express";
+// import  Jwt, { type JwtPayload }  from "jsonwebtoken";
+// import config from "../config";
+// import { pool } from "../db";
+// const auth = ()=>{
+//     return async (req : Request,res : Response,next : NextFunction)=>{
+
+//     // console.log(req.headers.authorization);
+
+//     const token = req.headers.authorization;
+//     console.log(token);
+    
+
+//     if(!token){
+//         res.status(401).json({
+//             success : false,
+//             message : "Unauthorized access!!",
+//         })
+//     }
+
+
+//     const decoded = Jwt.verify(token as string,config.secret as string) as JwtPayload;
+
+//     const userData = await pool.query(`
+        
+        
+//         SELECT * FROM users WHERE email=$1`, [decoded.email])
+
+//         // console.log(userData);
+
+//         const user = userData.rows[0];
+
+//         // console.log(user);
+        
+//         if(userData.rows.length === 0){
+//  res.status(404).json({
+//             success : false,
+//             message : "User Not Found!",
+//         });
+//         }
+
+
+//         if(user.is_active){
+//  res.status(403).json({
+//             success : false,
+//             message : "Forbidden!!",
+//         });
+//         }
+
+
+//     next();
+
+// };
+// }
+
+// export default auth
+
 import type { NextFunction, Request, Response } from "express";
 import  Jwt, { type JwtPayload }  from "jsonwebtoken";
 import config from "../config";
 import { pool } from "../db";
+
 const auth = ()=>{
     return async (req : Request,res : Response,next : NextFunction)=>{
 
@@ -12,10 +70,10 @@ const auth = ()=>{
     
 
     if(!token){
-        res.status(401).json({
+       return res.status(401).json({
             success : false,
             message : "Unauthorized access!!",
-        })
+        });
     }
 
 
@@ -24,7 +82,7 @@ const auth = ()=>{
     const userData = await pool.query(`
         
         
-        SELECT * FROM users WHERE email=$1`, [decoded.email])
+        SELECT * FROM users WHERE email=$1`, [decoded.email]);
 
         // console.log(userData);
 
@@ -33,9 +91,17 @@ const auth = ()=>{
         // console.log(user);
         
         if(userData.rows.length === 0){
- res.status(404).json({
+        return res.status(404).json({
             success : false,
             message : "User Not Found!",
+        });
+        }
+
+
+        if(!user.is_active){
+        return res.status(403).json({
+            success : false,
+            message : "Forbidden!!",
         });
         }
 
@@ -45,4 +111,4 @@ const auth = ()=>{
 };
 }
 
-export default auth
+export default auth;
